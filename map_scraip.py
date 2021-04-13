1f2fb1129c11b006734d31f8903ee9369df5eae9
ykto1234 <to.46101753@gmail.com>
2021-04-14 00:15:03 +0900

    Googleマップの取得処理修正

--- map_scraip.py
+++ map_scraip.py
@@ -19,6 +19,14 @@ import mylogger
 logger = mylogger.setup_logger(__name__)
 
 
+def scroll_down(driver, element):
+    for index in range(0, 5):
+        # ページの高さを取得
+        height = driver.execute_script("return arguments[0].scrollHeight", element)
+        driver.execute_script("arguments[0].scrollTo(0, " + str(height) + ");", element)
+        time.sleep(1.0)
+
+
 def search_google_map(driver, search_keyword, url):
     try:
         SEARCH_name = "q"
@@ -36,6 +44,7 @@ def search_google_map(driver, search_keyword, url):
 
         # 結果データリスト
         searchOutputInfoList = []
+        all_div_results = []
 
         # データがなかった時ようのハイフン
         NO_DATA_STR = '-'
@@ -135,7 +144,7 @@ def search_google_map(driver, search_keyword, url):
 
             # 結果リストを取得する
             div_results = []
-            DivResult_sel = "div.section-result"
+            DivResult_sel = "a.place-result-container-place-link"
             DivInfo_sel = "h1.section-hero-header-title-title"
             ButtonBack_sel = "button.section-back-to-list-button"
             NextBtn_xpath = '//*[@id="n7lv7yjyC35__section-pagination-button-next"]'
@@ -146,107 +155,28 @@ def search_google_map(driver, search_keyword, url):
             )
             # ページ遷移が上手くできていないことがあるため、チェック処理を加える
             if before_div_results:
-                before_storename = before_div_results[-1].select("h3")[0].text.strip()
+                before_a = before_div_results[-1]
                 for tmp in range(0, 30):
                     soup = BeautifulSoup(driver.page_source, features="html.parser")
                     after_div_results = soup.select(DivResult_sel)
                     if after_div_results:
-                        after_storename = after_div_results[-1].select("h3")[0].text.strip()
-                        if before_storename != after_storename:
+                        after_a = after_div_results[-1]
+                        if before_a != after_a:
                             break
 
                     time.sleep(1.0)
 
+            # スクロールを動かして最下部まで読み込ませる
+            scroll_sel = 'div.section-scrollbox'
+            scroll_ele = driver.find_elements_by_css_selector(scroll_sel)
+            scroll_down(driver, scroll_ele[1])
+
             soup = BeautifulSoup(driver.page_source, features="html.parser")
             div_results = soup.select(DivResult_sel)
+
             # 検索結果一覧を保持しておく
             before_div_results = div_results
-
-            for index in range(0, len(div_results)):
-                # ターゲット出現を待機
-                WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, DivResult_sel))
-                )
-                page_link = driver.find_elements_by_css_selector(DivResult_sel)[index]
-                page_link.click()
-
-                WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, DivInfo_sel))
-                )
-                detail_soup = BeautifulSoup(driver.page_source, features="html.parser")
-
-                # 初期値設定
-                _searchOutputInfo = SearchOutputInfo()
-                _searchOutputInfo.search_keyword = search_keyword
-                _searchOutputInfo.search_areaname = search_keyword.split(" ")[1]
-                _searchOutputInfo.storename = NO_DATA_STR
-                _searchOutputInfo.industry = NO_DATA_STR
-                _searchOutputInfo.address = NO_DATA_STR
-                _searchOutputInfo.tel_number = NO_DATA_STR
-                _searchOutputInfo.web_url = NO_DATA_STR
-
-                # 値の設定
-                if len(detail_soup.select("h1")):
-                    _searchOutputInfo.storename = detail_soup.select("h1")[0].text.strip()
-
-                if len(detail_soup.select("div.ugiz4pqJLAG__primary-text.gm2-body-2")):
-                    address_info = detail_soup.select("div.ugiz4pqJLAG__primary-text.gm2-body-2")[0].text
-                    if address_info:
-                        address_list = address_info.split(" ")
-                        # 郵便番号
-                        postal_code_pattern = r"\d{3}-\d{4}"
-                        postal_code_rx = re.compile(postal_code_pattern)
-                        # 住所
-                        address = ""
-                        for address_str in address_list:
-                            postal_code_mo =  postal_code_rx.search(address_str)
-                            if postal_code_mo:
-                                postal_code = postal_code_mo.group()
-                            else:
-                                address += address_str
-                        _searchOutputInfo.address = address
-                        _searchOutputInfo.postal_code = postal_code
-
-                if len(div_results[index].select("span.section-result-details")):
-                    _searchOutputInfo.industry = div_results[index].select("span.section-result-details")[0].text
-
-                for detail_content in driver.find_elements_by_css_selector("button.ugiz4pqJLAG__button"):
-                    aria_label = ""
-                    if detail_content:
-                        aria_label = detail_content.get_attribute("aria-label")
-                    # 電話番号
-                    if aria_label:
-                        tel_pattern = r"\d{2,4}-\d{2,4}-\d{2,4}"
-                        tel_number_rx = re.compile(tel_pattern)
-                        tel_number_mo =  tel_number_rx.search(aria_label)
-                        if tel_number_mo:
-                            tel_number_text = tel_number_mo.group().replace('-', '')
-                            _searchOutputInfo.tel_number = tel_number_text
-
-                action_byn_sel = "button.section-action-chip-button"
-                for action_button in driver.find_elements_by_css_selector(action_byn_sel):
-                    action_button_aria_label = ""
-                    if action_button:
-                        action_button_aria_label = action_button.get_attribute("aria-label")
-                    # ウェブサイト
-                    if action_button_aria_label and "ウェブサイトをコピーします" in action_button_aria_label:
-                        WebDriverWait(driver, 30).until(
-                            EC.element_to_be_clickable((By.CSS_SELECTOR, action_byn_sel))
-                        )
-                        driver.execute_script("arguments[0].click();", action_button)
-                        web_url = pyperclip.paste()
-                        _searchOutputInfo.web_url = web_url
-                        break
-
-                searchOutputInfoList.append(_searchOutputInfo)
-
-                # 結果一覧に戻るボタンを押す
-                WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, ButtonBack_sel))
-                )
-                back_button = driver.find_elements_by_css_selector(ButtonBack_sel)[0]
-                back_button.click()
-                continue
+            all_div_results.extend(div_results)
 
             # 次のページボタンが押せるか確認
             NextBtn_sel = "section-pagination-button-next"
@@ -266,6 +196,87 @@ def search_google_map(driver, search_keyword, url):
                 next_btn_ele = driver.find_element_by_xpath(NextBtn_xpath)
                 driver.execute_script("arguments[0].click();", next_btn_ele)
                 time.sleep(1.0)
+
+
+        for index in range(0, len(all_div_results)):
+
+            detail_page_link = all_div_results[index].get('href')
+            driver.get(detail_page_link)
+
+            WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, DivInfo_sel))
+            )
+            detail_soup = BeautifulSoup(driver.page_source, features="html.parser")
+
+            # 初期値設定
+            _searchOutputInfo = SearchOutputInfo()
+            _searchOutputInfo.search_keyword = search_keyword
+            _searchOutputInfo.search_areaname = search_keyword.split(" ")[1]
+            _searchOutputInfo.storename = NO_DATA_STR
+            _searchOutputInfo.industry = NO_DATA_STR
+            _searchOutputInfo.address = NO_DATA_STR
+            _searchOutputInfo.tel_number = NO_DATA_STR
+            _searchOutputInfo.web_url = NO_DATA_STR
+
+            # 値の設定
+            if len(detail_soup.select("h1")):
+                _searchOutputInfo.storename = detail_soup.select("h1")[0].text.strip()
+
+            if len(detail_soup.select("div.ugiz4pqJLAG__primary-text.gm2-body-2")):
+                address_info = detail_soup.select("div.ugiz4pqJLAG__primary-text.gm2-body-2")[0].text
+                if address_info:
+                    address_list = address_info.split(" ")
+                    # 郵便番号
+                    postal_code_pattern = r"\d{3}-\d{4}"
+                    postal_code_rx = re.compile(postal_code_pattern)
+                    # 住所
+                    address = ""
+                    for address_str in address_list:
+                        postal_code_mo =  postal_code_rx.search(address_str)
+                        if postal_code_mo:
+                            postal_code = postal_code_mo.group()
+                        else:
+                            address += address_str
+                    _searchOutputInfo.address = address
+                    _searchOutputInfo.postal_code = postal_code
+
+            indeustry_content = driver.find_element_by_xpath('//button[@jsaction="pane.rating.category"]')
+            if indeustry_content:
+                _searchOutputInfo.industry = indeustry_content.text
+
+            for detail_content in driver.find_elements_by_css_selector("button.ugiz4pqJLAG__button"):
+                aria_label = ""
+                if detail_content:
+                    aria_label = detail_content.get_attribute("aria-label")
+                # 電話番号
+                if aria_label:
+                    tel_pattern = r"\d{2,4}-\d{2,4}-\d{2,4}"
+                    tel_number_rx = re.compile(tel_pattern)
+                    tel_number_mo =  tel_number_rx.search(aria_label)
+                    if tel_number_mo:
+                        tel_number_text = tel_number_mo.group().replace('-', '')
+                        _searchOutputInfo.tel_number = tel_number_text
+
+            action_byn_sel = "button.section-action-chip-button"
+            for action_button in driver.find_elements_by_css_selector(action_byn_sel):
+                action_button_aria_label = ""
+                if action_button:
+                    action_button_aria_label = action_button.get_attribute("aria-label")
+                # ウェブサイト
+                if action_button_aria_label and "ウェブサイトをコピーします" in action_button_aria_label:
+                    WebDriverWait(driver, 30).until(
+                        EC.element_to_be_clickable((By.CSS_SELECTOR, action_byn_sel))
+                    )
+                    action_button.click()
+                    web_url = pyperclip.paste()
+                    _searchOutputInfo.web_url = web_url
+                    break
+
+            searchOutputInfoList.append(_searchOutputInfo)
+
+            continue
+
+        return searchOutputInfoList
 
     except TimeoutException:
         logger.error("TimeoutExceptionが発生したため、次の検索に移ります")
